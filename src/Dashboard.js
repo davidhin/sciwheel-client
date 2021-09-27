b52c3432d523c498199b153b534ff3b99316d1a8
David Hin <hello@davidhin.com>
2021-09-27 14:31:37 +0930

    Update dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,61 +1,29 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
+import TextField from '@mui/material/TextField';
 
-const drawerWidth = 240;
 
 export default function Dashboard() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ display: "flex", width: "100%" }}>
       <Drawer
         sx={{
-          width: drawerWidth,
-          flexShrink: 0,
+          width: 240,
           "& .MuiDrawer-paper": {
-            width: drawerWidth,
+            width: 240,
             boxSizing: "border-box",
+            p: 3,
           },
         }}
         variant="permanent"
         anchor="left"
       >
-        <Toolbar />
-        <List>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={"text"} />
-          </ListItem>
-        </List>
+        <Typography variant="body1">Tags</Typography>
       </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-      >
-        <Toolbar />
-
-        <Typography paragraph>Hello World</Typography>
+      <Box sx={{ p: 3, flexGrow: 1 }}>
+      <TextField fullWidth id="outlined-basic" label="Search" variant="outlined" />
       </Box>
     </Box>
   );
