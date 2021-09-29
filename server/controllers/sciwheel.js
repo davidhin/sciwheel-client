589d831921069c9006f1679f96cc2853f05c3777
David Hin <hello@davidhin.com>
2021-09-29 14:45:10 +0930

    Update sciwheel helpers

--- server/controllers/sciwheel.js
+++ server/controllers/sciwheel.js
@@ -2,13 +2,13 @@ const fetch = require("node-fetch");
 
 module.exports = {
   cookie: async function (username, password) {
-    springsecurity = "_spring_security_remember_me=true";
-    ret = await fetch("https://sciwheel.com/work/signinUser", {
+    let springsecurity = "_spring_security_remember_me=true";
+    let ret = await fetch("https://sciwheel.com/work/signinUser", {
       method: "POST",
       body: `j_username=${username}&j_password=${password}&${springsecurity}`,
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
     });
-    cookie = ret.headers.raw()["set-cookie"];
+    let cookie = ret.headers.raw()["set-cookie"];
     return cookie;
   },
 
@@ -35,10 +35,10 @@ module.exports = {
   },
 
   references: async function (cookie) {
-    page_1 = await module.exports.references_page(cookie, 1);
-    totalpages = Math.ceil(page_1["totalHitCount"] / 200);
-    references = page_1["displayedItems"];
-    pages = [...Array(totalpages + 1).keys()].slice(2);
+    let page_1 = await module.exports.references_page(cookie, 1);
+    let totalpages = Math.ceil(page_1["totalHitCount"] / 200);
+    let references = page_1["displayedItems"];
+    let pages = [...Array(totalpages + 1).keys()].slice(2);
     pages = await Promise.all(
       pages.map(async (h) => {
         return await module.exports.references_page(cookie, h);
@@ -46,5 +46,30 @@ module.exports = {
     );
     pages.forEach((page) => references.push(...page["displayedItems"]));
     return references;
+  },
+
+  comments_page: async function (cookie, page) {
+    return await fetch(
+      `https://sciwheel.com/work/api/collection/558788/comments?query=&resultsPerPage=100&page=${page}&useAuthorsFromSolr=true`,
+      {
+        method: "get",
+        headers: { cookie: cookie },
+      }
+    ).then((res) => res.json());
+  },
+
+  comments: async function (cookie) {
+    let page_1 = await module.exports.comments_page(cookie, 1);
+    let totalpages = Math.ceil(page_1["totalHitCount"] / 100);
+    let comments = page_1["displayedItems"];
+    let pages = [...Array(totalpages + 1).keys()].slice(2);
+    pages = await Promise.all(
+      pages.map(async (h) => {
+        return await module.exports.comments_page(cookie, h);
+      })
+    );
+    pages.forEach((page) => comments.push(...page["displayedItems"]));
+    console.log(pages.length, comments.length);
+    return comments;
   },
 };
